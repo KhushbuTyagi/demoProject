05cb71f8163a2d4865203a66ea70fa2bbe0d2e23
91904 <thekhushbutyagi@gmail.com>
2022-11-24 20:58:49 +0100

    Added new changes to project.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Button from '@mui/material/Button';
 import { Routes, Route } from 'react-router-dom';
 import Tasks from './todo-component/Tasks';
 function App() {
+
   return (
    <Routes>
+
      <Route path="/" element={<Tasks/>}/>
+
    </Routes>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 
-test('renders learn react link in demo app', () => {
+test('renders learn react link in the demo app', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
